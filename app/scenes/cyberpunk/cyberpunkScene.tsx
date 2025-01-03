1d1e6ad17b02a66cfc156ff4e6fe5349b8ce4f14
Bryce Bryce <bpearce@angelo.edu>
2025-01-02 21:28:24 -0800

    anotha one

--- app/scenes/cyberpunk/cyberpunkScene.tsx
+++ app/scenes/cyberpunk/cyberpunkScene.tsx
@@ -1,11 +1,11 @@
 import * as THREE from "three";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import FirstPersonControls from "~/controls/FirstPersonControls";
-import { EnergyPulses } from "./components/Grid/effects/energyPulses";
 import { Grid } from "./components/Grid";
 import { PerimeterFrame } from "./components/Grid/components/perimeterFrame";
+import { useGridPulseEffect } from "./components/Grid/hooks/useGridPulseEffect";
 import type { GridLine } from "~/types/gridTypes";
 
 function CyberpunkGrid() {
@@ -18,23 +18,8 @@ function CyberpunkGrid() {
   const gridDepth = 350;
   const spacing = 2;
 
-  useFrame((state) => {
-    const time = state.clock.getElapsedTime();
-    groupRef.current?.traverse((obj) => {
-      if (obj instanceof THREE.Mesh && obj.userData?.baseColor) {
-        const { baseColor, offset, emissiveBase } = obj.userData;
-        const pulse = (Math.sin((time + offset) * 1) + 1) * 0.5;
-        const brightnessFactor = 0.3 + pulse * 1;
-        const emissiveFactor = 0.5 + pulse * 1.5;
-
-        const newBaseColor = baseColor.clone().multiplyScalar(brightnessFactor);
-        const newEmissive = emissiveBase.clone().multiplyScalar(emissiveFactor);
-
-        (obj.material as THREE.MeshStandardMaterial).color = newBaseColor;
-        (obj.material as THREE.MeshStandardMaterial).emissive = newEmissive;
-      }
-    });
-  });
+  // sinewave effect to create some noise on the base grid
+  useGridPulseEffect(groupRef);
 
   return (
     <group ref={groupRef}>
